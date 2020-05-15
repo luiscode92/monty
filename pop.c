59a9e240efcc20aabfd13d2a73b371df0ff657aa
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-05-14 20:40:12 -0500

    fix

--- pop.c
+++ pop.c
@@ -9,17 +9,19 @@ void _pop(stack_t **stack, unsigned int line)
 {
 	stack_t *tmp;
 
-	if (*stack != NULL)
+	if (*stack)
 	{
 		tmp = *stack;
 		if ((*stack)->next)
+		{
 			(*stack)->next->prev = NULL;
+		}
 		*stack = (*stack)->next;
 		free(tmp);
 	}
 	else
 	{
-		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack", line);
+		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n", line);
 		free_all(stack);
 		exit(EXIT_FAILURE);
 	}
